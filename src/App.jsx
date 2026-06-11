de933abb74636ec98babc46f85ec0552a9a70295
joo777-y <johamdy54@gmail.com>
2026-06-11 21:36:03 +0300

    update frontend1

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import SignUpPage from "./pages/SignUpPage.jsx";
 import ApplyPage from "./pages/jobApplication.jsx";
 import ScrollToTop from "./components/ui/scrollToTop.jsx";
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute.jsx";
-import OtpPage from "./pages/OtpPage.jsx";
+import Page from "./pages/Page.jsx";
 
 // ─── API CONFIG ────────────────────────────────────────────────────────────────
 const API_BASE_URL = import.meta.env?.VITE_API_BASE_URL || "http://localhost:5000/api";
@@ -52,7 +52,7 @@ export default function App() {
           <Route path="/login-page" element={<LoginPage />}/>
           <Route path="/sign-up-page" element={<SignUpPage />}/>
           <Route path="/job-application" element={<ApplyPage />}/>
-          <Route path="/verify-otp" element={<OtpPage />} />
+          <Route path="/verify-otp" element={<Page />} />
 
 
           <Route
